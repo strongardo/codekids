e83e5ce6a321a8e1e9dceb943a63fdf27ccc12fc
Николай Леонов <strongardo@gmail.com>
2025-05-02 12:19:55 +0600

    fix login form: show errors

--- codekids/urls.py
+++ codekids/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='pages/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
